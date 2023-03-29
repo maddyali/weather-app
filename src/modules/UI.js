648f596ead7e701cd223d13a39f4dad64843d464
Maddy Ali <maddyoali@gmail.com>
2023-03-29 08:40:48 -0500

    Hide search result on initial load

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,12 +2,14 @@ const UI = (function () {
   function displaySearchResult(weatherData) {
     if (!weatherData) return;
 
+    const searchResult = document.getElementById("searchResult");
     const cityName = document.getElementById("cityName");
     const temp = document.getElementById("temp");
     const feelsLike = document.getElementById("feelsLike");
     const humidity = document.getElementById("humidity");
     const wind = document.getElementById("wind");
 
+    searchResult.style.visibility = "visible";
     cityName.textContent = `${weatherData.cityName}`;
     temp.textContent = `${weatherData.temp} °F`;
     feelsLike.textContent = `Feels like ${weatherData.feelsLike} °F`;
